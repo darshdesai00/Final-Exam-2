d4df0da2c6794027f1f70d4b71d2b76b0bc2941a
darshdesai00 <darshdesai50@icloud.com>
2025-12-11 16:17:51 -0800

    Working on Milestone 1

--- main.cpp
+++ main.cpp
@@ -2,7 +2,34 @@
 #include <string>
 using namespace std;
 
-// Initial commit
+// Milestone 1 
+
+// Node Struct
+struct Node {
+    string name;
+    string order;
+    Node * next;
+};
+
+// Front and Back
+Node * head = NULL;
+Node * tail = NULL;
+
+// Add to the Back
+Node * n = new Node;{
+n->name= name;
+n->order = order;
+n->next = NULL;
+}
+
+// Queue
+void printQueue() {
+    Node * temp = head;
+    cout << "Coffee Booth Queue: ";
+    while (temp != NULL) {
+
+    }
+
 
 int main() {
     
