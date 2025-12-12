a2a8e1100d799ea81e0ff7db234230f2d2d63616
darshdesai00 <darshdesai50@icloud.com>
2025-12-11 18:19:01 -0800

    Milestone 6

--- main.cpp
+++ main.cpp
@@ -7,18 +7,17 @@
 #include <stack>
 using namespace std;
 
-// Milestone 1 :
-// Node Struct
+// Milestone 1 : Coffee Booth Node Struct
 struct Node {
     string name;
     string order;
     Node * next;
 };
-// Front and Back
+
 Node * head = NULL;
 Node * tail = NULL;
 
-// Add to the Back
+// Add customers to coffee queue
 void enqueue(string name, string order) {
 Node * n = new Node;
 n->name= name;
@@ -33,7 +32,8 @@ if (head == NULL) {
     }
 }
 
-// Coffee Queue
+// Print Functions
+
 void printCoffeeQueue() {
     Node * temp = head;
     cout << "Coffee Booth Queue: ";
@@ -44,7 +44,6 @@ void printCoffeeQueue() {
     cout << endl;
 }
 
-// Muffin Booth (deque)
 void printMuffinQueue(const deque<string>& dq) {
     cout << "Mufffin Booth Queue: ";
     for (int i = 0; i < dq.size(); i++) {
@@ -53,7 +52,6 @@ void printMuffinQueue(const deque<string>& dq) {
     cout << endl;
 }
 
-// Bracelet Booth (Vector)
 void printBraceletQueue(const vector<string>& v) {
     cout << "Bracelet Booth Queue: ";
     for (int i = 0; i < v.size(); i++)
@@ -61,7 +59,6 @@ void printBraceletQueue(const vector<string>& v) {
     cout << endl;
 }
 
-// Sticker Booth (stack)
 void printStickerStack(stack<string> s) {
     cout << "Sticker Booth Queue: ";
     while (!s.empty()) {
@@ -71,6 +68,7 @@ void printStickerStack(stack<string> s) {
     cout << endl;
 }
 
+// Main Simulation
 int main() {
     srand(time(0));
 
@@ -80,23 +78,23 @@ int main() {
     string bracelets[] = {"Beaded Bracelet", "Charm Bracelet", "Friendship Tie", "Woven Bracelet"};
     string stickers[] = {"Star Sticker", "Smile Sticker", "Sparkle Sticker", "Lightning Sticker"};
 
-    // Initial Coffee Queue with (3 random customers)
+    // Initial Coffee Booth with (3 random customers)
     for (int i = 0; i < 3; i++) {
         enqueue(names[rand() % 5], drinks[rand() % 5]);
     }
     
-    // Initial Muffin Queue (Deque)
+    // Muffin Queue (Deque)
     deque<string> muffinQueue;
     for (int i = 0; i < 3; i++) {
         muffinQueue.push_back(names[rand() % 5] + string(" --> ") + muffins[rand() % 4]);
     }
 
-    // Initial Bracelet Booth (Vector)
+    // Bracelet Booth (Vector)
     vector<string>braceletQueue;
     for(int i = 0; i < 3; i++)
     braceletQueue.push_back(names[rand() % 5] + " --> " + bracelets[rand() % 4]);
     
-    // Initial Sticker Booth (stack
+    // Sticker Booth (stack
     stack<string> stickerStack;
     for (int i = 0; i < 3; i++) {
         stickerStack.push(names[rand() % 5] + string(" --> ") + stickers[rand() % 4]);
@@ -109,8 +107,8 @@ int main() {
         cout << "---- Round " << round << " ----\n";
     
         
-// *** COFFEE BOOTH ***
-        // Served customer at the head spot
+        // *** COFFEE BOOTH ***
+        
         if (head != NULL) {
             cout << "Coffee Served: " << head->name << " --> " << head->order << endl;
 
@@ -132,14 +130,15 @@ int main() {
             cout << "Cofffe new arrival: " << customerName << " --> " << customerOrder << endl;
         }
 
-// *** MUFFIN BOOTH ***
+    // *** MUFFIN BOOTH ***
+
         if (!muffinQueue.empty()) {
             cout << "Muffin Served: " << muffinQueue.front() << endl;
             muffinQueue.pop_front();
         } else {
             cout << "No customers at Muffin Booth.\n";
     }
-        // 50% chance
+
         if (rand() % 2 == 0) {
             string customerName = names[rand() % 5];
             string customerOrder = muffins[rand() % 4];
@@ -147,7 +146,8 @@ int main() {
             cout << "Muffin new arrival: " << customerName << " --> " << customerOrder << endl;
         }
 
-// *** BRACELET BOOTH ***
+    // *** BRACELET BOOTH ***
+
         if (!braceletQueue.empty()) {
             cout << "Bracelet Served: " << braceletQueue.front() << endl;
             braceletQueue.erase(braceletQueue.begin());
@@ -162,7 +162,8 @@ int main() {
             cout << "Bracelet new arrival: " << n << " --> " << b << endl;
         }
         
-// *** STICKER BOOTH ***
+        // *** STICKER BOOTH ***
+
         if (!stickerStack.empty()) {
             cout << "Sticker Served: " << stickerStack.top() << endl;
             stickerStack.pop();
@@ -177,6 +178,8 @@ int main() {
             cout << "Sticker new arrival: " << sn << " --> " << so << endl;
         }
        
+        // End of the round status
+        cout << "--- Queue Status After Round " << round << " ---\n";
         printCoffeeQueue();
         printMuffinQueue(muffinQueue);
         printBraceletQueue(braceletQueue);
